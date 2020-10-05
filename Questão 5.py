4d7774ed833b921de792d90fc071b55f2fd8858b
VictorBarcelos <61838204+VictorBarcelos@users.noreply.github.com>
2020-10-05 12:22:29 -0300

    Update Questão 5.py

--- Questão 5.py
+++ Questão 5.py
@@ -1,19 +1,17 @@
-menor_q_160 = entre_160_e_180 = maior_q_180 = 0
+menor160 = maior180 = entre160e180 = 0
 
-for cont in range(3):
-    altura = float(input('Digite sua altura: '))
+for calc in range(3):
+    altura = float(input(f'Digite a altura da {calc + 1}º pessoa: '))
 
-    if altura < 1.60:
-        menor_q_160 =+ 1
+    if altura < 1.6:
+        menor160 += 1
+    elif altura > 1.8:
+        maior180 += 1
+    elif 1.6 <= altura <= 1.80:
+        entre160e180 += 1
 
-    elif 1.80 >= altura >= 1.60:
-        entre_160_e_180 =+ 1
-
-    else:
-        maior_q_180 =+ 1
-
-print("--------------------------------------------")
-print("Menores que 1.60m:",menor_q_160)
-print("Entre 1.60m e 1.80m:",entre_160_e_180)
-print("Maiores que 1.80m:",maior_q_180)
-print("--------------------------------------------")
+print("-----------------------------------------")
+print(f'Menores a 1.60 foram {menor160}')
+print(f'Maiores a 1.80 foram {maior180}')
+print(f'Entre 1.60 e 1.80 foram {entre160e180}')
+print("-----------------------------------------")
