6fafc760cb157dea052cee7c7c49e4ddd8456df5
VictorBarcelos <61838204+VictorBarcelos@users.noreply.github.com>
2020-10-05 12:05:20 -0300

    Update Questão 2.py

--- Questão 2.py
+++ Questão 2.py
@@ -1,7 +1,7 @@
-vitoria=int(input('Vitórias:'))
-empate=int(input('Empates:'))
-derrotas=int(input("Derrotas:"))
-pontos_totais=vitoria*3+empate
-n_de_jogos=vitoria+empate+derrotas
-print('Quantidade de Jogos:',n_de_jogos)
-print('Pontos Obtidos:',pontos_totais)
+vitoria = int(input('Digite o número de jogos vitoriosos: '))
+empate = int(input('Digite o número de empates: '))
+derrota = int(input('Digite o número de derrotas: '))
+jogos = vitoria + empate + derrota
+pontos_ganhos = vitoria*3
+perdidos = derrota*3 + empate*2
+print(f'O time jogou {jogos} jogos e obteve {pontos_ganhos} pontos e teve {perdidos} pontos peridos')
